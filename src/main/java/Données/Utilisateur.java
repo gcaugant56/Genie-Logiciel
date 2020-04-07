f3a30d522361d73d8b08c9d313405104cb22c3d0
Matteo Quintero <quintero@et.esiea.fr>
2020-04-07 17:19:21 +0200

    Début json

---
+++ src/main/java/Données/Utilisateur.java
@@ -0,0 +1,13 @@
+package Données;
+
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private int num;
+
+    public Utilisateur (String nom, String prenom, String message){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.num = num;
+    }
+}
