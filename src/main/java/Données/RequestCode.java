1d8237547084fba35188604ebfaa84fbffe3d97a
armelito60 <tchiasso@et.esiea.fr>
2020-04-30 21:31:09 +0200

    Enumération faite - modif

--- src/main/java/Données/RequestCode.java
+++ src/main/java/Données/RequestCode.java
@@ -1,4 +1,25 @@
 package Données;
 
 public enum RequestCode {
+
+    CREATION_COMPTE(1),
+    CONNEXION_CHAT(2),
+    DECONNEXION(3),
+    ENVOI_MSG(4),
+    MODIF_MDP(5),
+    MODIF_USERNAME(6),
+    AJOUT_CONTACT(7),
+    CREATION_GROUP(8),
+    ENVOI_GROUP(9);
+
+    private int numero;
+
+    RequestCode(int numero) {
+        this.numero = numero;
+    }
+
+    public String toString() {
+        return Integer.toString(this.numero);
+    }
+
 }
