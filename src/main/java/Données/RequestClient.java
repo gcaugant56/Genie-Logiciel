2eeba9c071323cd30dcf76cea460abe80057345e
armelito60 <tchiasso@et.esiea.fr>
2020-05-02 18:10:09 +0200

    interface requestcode complété avec les méthodes adaptées

--- src/main/java/Données/RequestClient.java
+++ src/main/java/Données/RequestClient.java
@@ -4,5 +4,11 @@ import java.io.IOException;
 
 public interface RequestClient {
     public boolean createAccount() throws IOException;
-
+    public boolean chatConnect();
+    public boolean chatPlugDisconnect();
+    public String sendMsg();
+    public boolean modifPassword();
+    public boolean modifUsername();
+    public boolean addContact();
+    public boolean createGroup();
 }
