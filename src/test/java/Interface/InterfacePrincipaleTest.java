e1d7adfd1fe7b0c6034be93cac19f2495c7c515d
Valentin camozzi <camozzi@et.esiea.fr>
2020-05-10 22:46:16 +0200

    ajout Junit 5

---
+++ src/test/java/Interface/InterfacePrincipaleTest.java
@@ -0,0 +1,18 @@
+package Interface;
+
+import Donnees.Utilisateur;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InterfacePrincipaleTest {
+
+    @Test
+    public void doNothing(){
+        Utilisateur toto = null;
+            (new InterfacePrincipale(toto)
+
+        }
+    }
+}
